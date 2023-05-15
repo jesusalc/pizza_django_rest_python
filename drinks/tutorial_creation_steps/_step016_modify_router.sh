05845a6727614f19d4bfe5042c34346bc39a1647
jesusalc <jesusalc@gmail.com>
2023-05-15 20:21:28 +0200

    saved more steps on basic generator for crud and show json

--- drinks/tutorial_creation_steps/_step016_modify_router.sh
+++ drinks/tutorial_creation_steps/_step016_modify_router.sh
@@ -1,6 +1,7 @@
-"""drinks URL Configuration
+touch drinks/urls.py
+echo "\"\"\"drinks URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The \`urlpatterns\` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
@@ -12,7 +13,8 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+\"\"\"
+
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -21,3 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('drinks/', views.drink_list),
 ]
+
+
+" > drinks/urls.py
+
