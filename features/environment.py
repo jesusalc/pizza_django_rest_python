837010d84e1b871a4700f4d2fd5400407b423687
jesusalc <jesusalc@gmail.com>
2023-05-16 08:54:11 +0200

    fixed bdd setup again and have working failing bdd cases

--- features/environment.py
+++ features/environment.py
@@ -3,18 +3,14 @@ This module contains setup and teardown methods for the behave environment.
 """
 
 import os
+import django
 from behave import use_fixture
 from django.test.runner import DiscoverRunner
-from django.conf import settings
 
-# This is a fixture to ensure Django is set up for the tests
-def django_test_runner(context):
-    context.test_runner = DiscoverRunner()
-    context.test_runner.setup_test_environment()
-    context.old_db_config = context.test_runner.setup_databases()
+os.environ["DJANGO_SETTINGS_MODULE"] = "pizza_django.settings"  # replace 'your_project.settings' with your actual settings
 
 def before_all(context):
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'pizza_django.settings'
+    django.setup()
     context.test_runner = DiscoverRunner()
     context.test_runner.setup_test_environment()
     context.old_db_config = context.test_runner.setup_databases()
